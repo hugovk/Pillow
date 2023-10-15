90db1efcc32946cf37900ac32147898fb845bd6a
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-10-15 11:17:37 +0300

    Time installation commands

--- .ci/install.sh
+++ .ci/install.sh
@@ -20,30 +20,30 @@ fi
 set -e
 
 if [[ $(uname) != CYGWIN* ]]; then
-    sudo apt-get -qq install libfreetype6-dev liblcms2-dev python3-tk\
+    sudo time apt-get -qq install libfreetype6-dev liblcms2-dev python3-tk\
                              ghostscript libffi-dev libjpeg-turbo-progs libopenjp2-7-dev\
                              cmake meson imagemagick libharfbuzz-dev libfribidi-dev\
                              sway wl-clipboard libopenblas-dev
 fi
 
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade wheel
-PYTHONOPTIMIZE=0 python3 -m pip install cffi
-python3 -m pip install coverage
-python3 -m pip install defusedxml
-python3 -m pip install olefile
-python3 -m pip install -U pytest
-python3 -m pip install -U pytest-cov
-python3 -m pip install -U pytest-timeout
-python3 -m pip install pyroma
+time python3 -m pip install --upgrade pip
+time python3 -m pip install --upgrade wheel
+time PYTHONOPTIMIZE=0 python3 -m pip install cffi
+time python3 -m pip install coverage
+time python3 -m pip install defusedxml
+time python3 -m pip install olefile
+time python3 -m pip install -U pytest
+time python3 -m pip install -U pytest-cov
+time python3 -m pip install -U pytest-timeout
+time python3 -m pip install pyroma
 
 if [[ $(uname) != CYGWIN* ]]; then
-    python3 -m pip install numpy
+    time python3 -m pip install numpy
 
     # PyQt6 doesn't support PyPy3
     if [[ $GHA_PYTHON_VERSION == 3.* ]]; then
-        sudo apt-get -qq install libegl1 libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxkbcommon-x11-0
-        python3 -m pip install pyqt6
+        sudo time apt-get -qq install libegl1 libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxkbcommon-x11-0
+        time python3 -m pip install pyqt6
     fi
 
     # webp
